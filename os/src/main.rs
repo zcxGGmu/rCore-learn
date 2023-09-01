50da1a9f33f6e82a45f62f0dfe4ba8eb9f6aee26
MASKER123 <zhouquan1511@163.com>
2023-09-01 10:58:44 +0800

    code a simplelogger on log

--- os/src/main.rs
+++ os/src/main.rs
@@ -1,21 +1,16 @@
 #![feature(panic_info_message)]
 #![no_main]
 #![no_std]
+
 #[macro_use]
 mod console;
 mod lang_items;
 mod sbi;
-
+mod logging;
 
 use core::arch::global_asm;
+use log::{*};
 global_asm!(include_str!("entry.asm"));
-
-#[no_mangle]
-pub fn rust_main() -> ! {
-    clear_bss();
-    println!("hello, rCore!");
-    panic!("shutdown machine!");
-}
 
 fn clear_bss() {
     extern "C" {
@@ -28,4 +23,41 @@ fn clear_bss() {
             (a as *mut u8).write_volatile(0)
         }
     });
+}
+
+#[no_mangle]
+pub fn rust_main() -> ! {
+    extern "C" {
+        fn stext();
+        fn etext();
+        fn srodata();
+        fn erodata();
+        fn sdata();
+        fn edata();
+        fn sbss();
+        fn ebss();
+        fn boot_stack_lower_bound();
+        fn boot_stack_top();
+    }
+    clear_bss();
+
+    logging::init();
+    warn!("Deallocate frame: {:#x}",
+          stext as usize);
+    info!(".text [{:#x}, {:#x})",
+            stext as usize, 
+            etext as usize);
+    debug!(".rodata [{:#x}, {:#x})",
+            srodata as usize,
+            erodata as usize);
+    trace!(".data [{:#x}, {:#x})",
+            sdata as usize,
+            edata as usize);
+    error!("boot_stack [{:#x}, {:#x})",
+            boot_stack_lower_bound as usize,
+            boot_stack_top as usize);
+    println!(".bss [{:#x}, {:#x})",
+            sbss as usize,
+            ebss as usize);
+    panic!("Shutdown machine!!!");
 }
