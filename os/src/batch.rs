5b18465687805e22363e5d5a8127059821021ed4
MASKER123 <zhouquan1511@163.com>
2023-09-06 16:37:25 +0800

    realize batch-os core function :{first these app bins store in os.data, and after os run to load files to 0x80400000}

---
+++ os/src/batch.rs
@@ -0,0 +1,85 @@
+//! batch subsystem
+
+use crate::sync::UPSafeCell;
+use core::arch::asm;
+use lazy_static::*;
+
+const MAX_APP_NUM: usize = 16;
+const APP_BASE_ADDRESS: usize = 0x80400000;
+const APP_SIZE_LIMIT: usize = 0x20000;
+
+struct AppManager {
+   num_app: usize,
+   current_app, usize,
+   app_start: [usize; MAX_APP_NUM + 1],
+}
+
+impl AppManager {
+    pub fn get_current_app(&self) -> usize {
+        self.current_app
+    }
+
+    pub fn move_to_next_app(&self) {
+        self.current_app += 1;
+    }
+
+    pub fn print_app_info(&self) {
+        println!("[kernel] num_app = {}", self.num_app);
+        for i in 0..self.num_app {
+           println!(
+               "[kernel] app_{} [:#x], {:#x}",
+               i,
+               self.app_start[i],
+               self.app_start[i + 1]
+           ); 
+        }
+    }
+
+    fn load_app(&self, app_id: usize) {
+        if app_id >= self.num_app {
+            panic!("All applications completed!");
+        }
+        println!("[kernel] Loading app_{}", app_id);
+        
+        //clear app area
+        core::slice::from_raw_parts_mut(
+            APP_BASE_ADDRESS as *mut u8;
+            APP_SIZE_LIMIT
+        ).fill(0);
+        let app_src = core::slice:;from_raw_parts(
+            self.app_start[app_id] as *const u8,
+            self.app_start[app_id + 1] - self.app_start[app_id]
+        );
+        let app_dst = core::slice::from_raw_parts_mut(
+            APP_BASE_ADDRESS as *mut u8,
+            app_src.len()
+        );
+        app_dst.copys_from_slice(app_src);
+
+        // memory fence about fetching the instruction memory
+        asm!("fence.i");
+    }
+}
+
+lazy_static! {
+    static ref APP_MANAGER: UPSafeCell<AppManager> = unsafe {
+        UPSafeCell::new(
+            {
+                extern "C" {
+                    fn _num_app();
+                }
+                let num_app_ptr = _unm_app as usize as *const usize;
+                let num_app = num_app_ptr.read_volatile();
+                let mut app_start: [usize; MAX_APP_NUM + 1] = [0; MAX_APP_NUM + 1];
+                let app_start_raw: &[usize] = 
+                    core::slice::from_raw_parts(num_app_ptr.add(1), num_app + 1);
+                app_start[..=num_app].copy_from_slice(app_start_raw);
+                AppManager {
+                    num_app,
+                    current_app: 0,
+                    app_start,
+                }
+            }
+        )
+    };
+}
