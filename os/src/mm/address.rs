c4226be110f0f93e27da5d34c7e899a90454eba6
zq <zhouquan1511@163.com>
2023-10-12 14:43:38 +0800

    rCore: mm: Implementation of SimpleRange<T> for `VPNRange`
    "VPNRange" describes a continuous range of VirtPageNum,
    indicating the position and length of a logical segment
    within an address space. It functions as an iterator,
    allowing iteration using Rust's for-loop syntax.
    
    Signed-off-by: zq <zhouquan1511@163.com>

--- os/src/mm/address.rs
+++ os/src/mm/address.rs
@@ -205,3 +205,103 @@ impl VirtPageNum {
         idx
     }
 }
+
+/*
+    The data structures on which "vpnrange" relies
+*/
+pub trait StepByOne {
+    fn step(&mut self);
+}
+
+impl StepByOne for VirtPageNum {
+    fn step(&mut self) {
+        self.0 += 1;
+    }
+}
+
+impl StepByOne for PhysPageNum {
+    fn step(&mut self) {
+        self.0 += 1;
+    }
+}
+
+/// SimpleRange<T>
+#[derive(Copy, Clone)]
+pub struct SimpleRange<T>
+where
+    T: StepByOne + Copy + PartialEq + PartialOrd + Debug,
+{
+    l: T,
+    r: T,
+}
+
+impl<T> SimpleRange<T>
+where
+    T: StepByOne + Copy + PartialEq + PartialOrd + Debug,
+{
+    pub fn new(start: T, end: T) -> Self {
+        assert!(start <= end, "start {:?} ? end {:?} is error!", start, end);
+        Self {
+            l: start,
+            r: end,
+        }
+    }
+    
+    pub fn get_start(&self) -> T {
+        self.l
+    }
+
+    pub fn get_end(&self) -> T {
+        self.r
+    }
+}
+
+/// Iterator of SimpleRange<T>
+pub struct SimpleRangeIterator<T>
+where
+    T: StepByOne + Copy + PartialEq + PartialOrd + Debug,
+{
+    current: T,
+    end: T,
+}
+
+impl<T> SimpleRangeIterator<T>
+where
+    T: StepByOne + Copy + PartialEq + PartialOrd + Debug,
+{
+    pub fn new(l: T, r: T) -> Self {
+        Self {
+            current: l,
+            end: r,
+        }
+    }
+}
+
+impl<T> Iterator for SimpleRangeIterator<T>
+where
+    T: StepByOne + Copy + PartialEq + PartialOrd + Debug,
+{
+    type Item = T;
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.current == self.end {
+            None
+        } else {
+            let v = self.current;
+            self.current.step();
+            Some(v)
+        }
+    }
+}
+
+impl<T> IntoIterator for SimpleRange<T>
+where
+    T: StepByOne + Copy + PartialEq + PartialOrd + Debug,
+{
+    type Item = T;
+    type IntoIter = SimpleRangeIterator<T>;
+    fn into_iter(self) -> Self::IntoIter {
+        SimpleRangeIterator::new(self.l, self.r)
+    }
+}
+
+pub type VPNRange = SimpleRange<VirtPageNum>;
