c1a8de1adaab2915eb7c291ebc173b32e174b00c
zq <zhouquan1511@163.com>
2023-10-13 23:30:18 +0800

    rCore: mm: Update previous trap process based on address space
    There are two main changes:
    1) some extensions to the 'TrapContext' structure.
    2) For the Trap mechanism, the core codes is modified
       after satp is enabled, these codes include __alltraps
       and __restore.
    
    Signed-off-by: zq <zhouquan1511@163.com>

--- os/src/trap/context.rs
+++ os/src/trap/context.rs
@@ -1,10 +1,32 @@
+//! Implementation of TrapContext
+
 use riscv::register::sstatus::{self, Sstatus, SPP};
 
 #[repr(C)]
+/**
+    Currently, sscratch only saves the pointer to the TrapContext structure.
+    Therefore, TrapContext must provide necessary information for application
+    and kernel switching, such as {kernel_satp/kernel_sp/trap_handler}. Once
+    they are written, they will not be changed again.
+*/
 pub struct TrapContext {
-    pub x: [usize; 32], //x0~x31
-    pub sstatus: Sstatus, //CSR sstatus
-    pub sepc: usize, //CSR sepc
+    /// general regs => x0~x31
+    pub x: [usize; 32],
+    
+    /// CSR sstatus
+    pub sstatus: Sstatus,
+    
+    /// CSR sepc
+    pub sepc: usize,
+
+    /// Base address of PageTabe
+    pub kernel_satp: usize,
+
+    /// Kernel stack
+    pub kernel_sp: usize,
+    
+    /// Address of trap_handler
+    pub trap_handler: usize,
 }
 
 impl TrapContext {
@@ -12,15 +34,25 @@ impl TrapContext {
         self.x[2] = sp;
     }
 
-    pub fn app_init_context(entry: usize, sp: usize) -> Self {
+    /// return initial TrapContext of application
+    pub fn app_init_context(
+        entry: usize,
+        sp: usize,
+        kernel_satp: usize,
+        kernel_sp: usize,
+        trap_handler: usize,
+    ) -> Self {
         let mut sstatus = sstatus::read();
-        sstatus.set_spp(SPP::User);
+        sstatus.set_spp(SPP::User); // we will ret to user!
         let mut cx = Self {
             x: [0; 32],
             sstatus,
             sepc: entry,
+            kernel_satp,
+            kernel_sp,
+            trap_handler,
         };
-        cx.set_sp(sp);
+        cx.set_sp(sp); // set user sp of application
         cx
     }
 }
