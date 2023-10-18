7dbb6b989336b656e7dcaa5303a9f8a24b68ccd3
zq <zhouquan1511@163.com>
2023-10-18 17:27:10 +0800

    rCore: user: Add some syscall functions about process management
    includes fork/exec/waitpid/getpid/read/sleep syscall interfaces.
    
    Signed-off-by: zq <zhouquan1511@163.com>

--- user/src/lib.rs
+++ user/src/lib.rs
@@ -36,6 +36,10 @@ fn clear_bss() {
     );
 }
 
+pub fn read(fd: usize, buf: &mut [u8]) -> isize {
+    sys_read(fd, buf)
+}
+
 pub fn write(fd: usize, buf: &[u8]) -> isize {
     sys_write(fd, buf)
 }
@@ -50,4 +54,49 @@ pub fn yield_() -> isize {
 
 pub fn get_time() -> isize {
     sys_get_time()
+}
+
+pub fn getpid() -> isize {
+    sys_getpid()
+}
+
+pub fn fork() -> isize {
+    sys_fork()
+}
+
+pub fn exec(path: &str) -> isize {
+    sys_exec(path)
+}
+
+/// waiting for any child process
+pub fn wait(exit_code: &mut i32) -> isize {
+    loop {
+        match sys_waitpid(-1, exit_code as *mut _) {
+            -2 => {
+                yield_();
+            },
+            // -1 or real pid
+            exit_pid => return exit_pid,
+        } 
+    }
+}
+
+/// waiting for a specified child process
+pub fn waitpid(pid: usize, exit_code: &mut i32) -> isize {
+    loop {
+        match sys_waitpid(pid as isize, exit_code as *mut _) {
+            -2 => {
+                yield_();
+            }
+            // -1 or a real pid
+            exit_pid => return exit_pid,
+        }
+    }
+}
+
+pub fn sleep(period_ms: usize) {
+    let start: isize = sys_get_time();
+    while sys_get_time() < start + period_ms as isize {
+        sys_yield();
+    }
 }
--- user/src/syscall.rs
+++ user/src/syscall.rs
@@ -1,9 +1,14 @@
 use core::arch::asm;
 
+const SYSCALL_READ: usize = 63;
 const SYSCALL_WRITE: usize = 64;
 const SYSCALL_EXIT: usize = 93;
 const SYSCALL_YIELD: usize = 124;
 const SYSCALL_GET_TIME: usize = 169;
+const SYSCALL_GETPID: usize = 172;
+const SYSCALL_FORK: usize = 220;
+const SYSCALL_EXEC: usize = 221;
+const SYSCALL_WAITPID: usize = 260;
 
 fn syscall(id: usize, args: [usize; 3]) -> isize {
     let mut ret: isize;
@@ -17,6 +22,13 @@ fn syscall(id: usize, args: [usize; 3]) -> isize {
         };
     }
     ret
+}
+
+pub fn sys_read(fd: usize, buffer: &mut [u8]) -> isize {
+   syscall(
+       SYSCALL_READ,
+       [fd, buffer.as_mut_ptr() as usize, buffer.len()]
+    )
 }
 
 pub fn sys_write(fd: usize, buffer: &[u8]) -> isize {
@@ -33,4 +45,52 @@ pub fn sys_yield() -> isize {
 
 pub fn sys_get_time() -> isize {
     syscall(SYSCALL_GET_TIME, [0, 0, 0])
+}
+
+pub fn sys_getpid() -> isize {
+    syscall(SYSCALL_GETPID, [0, 0, 0])
+}
+
+/**
+    Function: The current process forks out a child process.
+    Return value: 0 for child processes and PID for current processes.
+    syscall ID：220
+*/ 
+pub fn sys_fork() -> isize {
+    syscall(SYSCALL_FORK, [0, 0, 0])
+}
+
+/**
+    Function: Empty the address space of the current process and load a specific executable file,
+              return to user mode and start its execution.
+    
+    Parameter: path gives the name of the executable to be loaded;
+    
+    Return value: -1 if an error occurs (such as not finding an executable with a matching name),
+                  otherwise it should not be returned.
+    
+    syscall ID：221
+*/
+pub fn sys_exec(path: &str) -> isize {
+    syscall(SYSCALL_EXEC, [path.as_ptr() as usize, 0, 0])
+}
+
+/**
+    Function: The current process waits for a child process to become a zombie process,
+              reclaims all its resources and collects its return value.
+    
+    Parameter: pid indicates the process ID of the child process to wait, if -1 means
+               waiting for any child process;
+    
+    exit_code: indicates the address where the return value of the child process is saved,
+              and if this address is 0, it does not have to be saved.
+    
+    Return value: -1 if the child process to wait does not exist; otherwise -2 if none of
+                  the child processes to wait for have ended; Otherwise, the process ID
+                  of the ending child process is returned.
+
+    syscall ID：260
+*/
+pub fn sys_waitpid(pid: isize, exit_code: *mut i32) -> isize {
+    syscall(SYSCALL_WAITPID, [pid as usize, exit_code as usize, 0])
 }
