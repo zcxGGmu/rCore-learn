c6145ab40f7e2278c4da8db500a5fa53e3c32bc0
zq <zhouquan1511@163.com>
2023-10-26 16:59:11 +0800

    rCore: fs: Implementation of [easy-fs/layout] `SuperBlock`
    SuperBlock describes the disk layout:
    |*super_block*|*inode_bitmap*|*inode_area*|*data_bitmap*|*data_area*|
    
    Signed-off-by: zq <zhouquan1511@163.com>

---
+++ easy-fs/src/layout.rs
@@ -0,0 +1,53 @@
+//!Rom layout
+//![super_block][inode_bitmap][inode_area][data_bitmap][data_area]
+use alloc::sync::Arc;
+use alloc::vec::Vec;
+use core::fmt::{Debug, Formatter, Result};
+
+const EFS_MAGIC: u32 = 0x3b800001;
+
+/// super_block
+#[repr(C)]
+pub struct SuperBlock {
+    magic: u32,
+    pub total_blocks: u32,
+    pub inode_bitmap_blocks: u32,
+    pub inode_area_blocks: u32,
+    pub data_bitmap_blocks: u32,
+    pub data_area_blocks: u32,
+}
+
+impl SuperBlock {
+    pub fn initialize(
+        &mut self,
+        total_blocks: u32,
+        inode_bitmap_blocks: u32,
+        inode_area_blocks: u32,
+        data_bitmap_blocks: u32,
+        data_area_blocks: u32,
+    ) {
+        *self = Self {
+            total_blocks,
+            inode_bitmap_blocks,
+            inode_area_blocks,
+            data_bitmap_blocks,
+            data_area_blocks,
+        }
+    }
+
+    pub fn is_valid(&self) -> bool {
+        self.magic == EFS_MAGIC
+    }
+}
+
+impl Debug for SuperBlock {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        f.debug_struct("SuperBlock")
+            .field("total_blocks", &self.total_blocks)
+            .field("inode_bitmap_blocks", &self.inode_bitmap_blocks)
+            .field("inode_area_blocks", &self.inode_area_blocks)
+            .field("data_bitmap_blocks", &self.data_bitmap_blocks)
+            .field("data_area_blocks", &self.data_area_blocks)
+            .finish()
+    }
+}
--- easy-fs/src/lib.rs
+++ easy-fs/src/lib.rs
@@ -4,12 +4,14 @@
 extern crate alloc;
 mod block_dev;
 mod block_cache;
+mod layout;
 
 use block_dev::BlockDevice;
 use block_cache::{
     get_block_cache,
     block_cache_sync_all,
 };
+use layout::*;
 
 pub const BLOCK_SZ: usize = 512;
 
