16d2bb9d8382bc5c2b6ed72ebabb883ef7e1be15
zq <zhouquan1511@163.com>
2023-10-26 16:01:43 +0800

    rCore: fs: Implementation of [easy-fs] block_cache
    A block cache and a global block cache manager are designed,
    where the block cache corresponds to a block of the underlying
    block device. All data structures of 'easy-fs' will first fall
    into the cache layer and then be uniformly written back to the
    block device by the cache layer.
    
    Signed-off-by: zq <zhouquan1511@163.com>

--- easy-fs/src/lib.rs
+++ easy-fs/src/lib.rs
@@ -3,3 +3,14 @@
 #![deny(missing_doc)]
 extern crate alloc;
 mod block_dev;
+mod block_cache;
+
+use block_dev::BlockDevice;
+use block_cache::{
+    get_block_cache,
+    block_cache_sync_all,
+};
+
+pub const BLOCK_SZ: usize = 512;
+
+
