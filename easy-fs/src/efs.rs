b8e5fd64dd610d76a01f5fd250351d5433bffb64
zq <zhouquan1511@163.com>
2023-10-29 11:53:57 +0800

    rCore: fs: Only add two files: {efs/vfs}.rs
    Signed-off-by: zq <zhouquan1511@163.com>

---
+++ easy-fs/src/efs.rs
@@ -0,0 +1,8 @@
+/*!
+The EasyFileSystem knows where each layout area is located,
+and disk block allocation and reclamation can be completed 
+through it, so it can be regarded as a disk block manager 
+in a sense.
+*/
+
+//TODO
---
+++ easy-fs/src/vfs.rs
@@ -0,0 +1,10 @@
+//!Virtaul File System
+/*!
+vfs, as the top layer of the file system model, can mask 
+the differences of the underlying disk layout and provide 
+an abstract interface for external users to read and write 
+files directly. Any file form that users see or use is 
+abstracted as [Inode].
+*/
+
+//TODO
